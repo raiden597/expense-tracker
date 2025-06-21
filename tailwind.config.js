f15d36d80669db4036777e85cf0ae4ee50643005
unknown <saran.123.1966@gmail.com>
2025-06-21 16:30:55 +0530

    charts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'color-empty',
+    'color-scale-1',
+    'color-scale-2',
+    'color-scale-3',
+    'color-scale-4',
+  ],
   theme: {
     extend: {},
   },
